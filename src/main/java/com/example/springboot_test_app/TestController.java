14a1a0bd7f95feb7a8e2416acbfa789e49de8a14
sarthak <sarthakgaonkar02@Sarthaks-Laptop.local>
2024-05-25 15:13:40 +0530

    updated

--- src/main/java/com/example/springboot_test_app/TestController.java
+++ src/main/java/com/example/springboot_test_app/TestController.java
@@ -1,3 +1,5 @@
+package com.example.springboot_test_app;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
