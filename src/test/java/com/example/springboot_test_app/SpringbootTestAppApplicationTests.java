94da892385208c1b0048648391619517ca4e28a3
sarthak <sarthakgaonkar02@Sarthaks-Laptop.local>
2024-05-25 12:56:27 +0530

    initial commit

---
+++ src/test/java/com/example/springboot_test_app/SpringbootTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_test_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
